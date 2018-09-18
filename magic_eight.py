93d7650d62c628600a1029e5dd72dc87d2d342d8
Yichuan <kerhyichuan@hotmail.com>
2018-09-18 17:05:12 -0400

    add_questions C

--- magic_eight.py
+++ magic_eight.py
@@ -3,4 +3,68 @@
 def question_from_user():
 	print("What is your question?")
 	question = input()
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def add_questionsC():
+    answer = random.randint(11,20)
+
+        if answer==11:
+            print ("Reply hazy, try again")
+
+        elif answer==12:
+            print ("Ask again later.")
+
+        elif answer==13:
+            print ("Better not tell you now.")
+
+        elif answer==14:
+            print ("Cannot predict now.")
+
+        elif answer==15:
+            print ("Concentrate and ask again.")
+
+        elif answer==16:
+            print ("Don't count on it.")
+
+        elif answer==17:
+            print ("My reply is no.")
+
+        elif answer==18:
+            print ("My sources say no.")
+
+        elif answer==19:
+            print ("Outlook not so good.")
+
+        elif answer==20:
+            print ("Very doubtful.")
+
